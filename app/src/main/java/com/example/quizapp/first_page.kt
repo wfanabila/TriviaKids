beaccef3792b5a02382de9a20af17dd62f112119
wfanabila <wfnabila77@gmail.com>
2026-01-10 05:51:47 +0800

    fix errors

--- app/src/main/java/com/example/quizapp/first_page.kt
+++ app/src/main/java/com/example/quizapp/first_page.kt
@@ -7,24 +7,24 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.quizapp.R
 class Firstpage : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.firstpage)
-
-        val btnGuest = findViewById<Button>(R.id.btnGuest)
-        val btnLogin = findViewById<Button>(R.id.btnLogin)
-        val tvSignUp = findViewById<TextView>(R.id.tvSignUp)
-
-        btnGuest.setOnClickListener {
-            // TODO: Navigate as guest
-        }
-
-        btnLogin.setOnClickListener {
-            // TODO: Navigate to LoginActivity
-        }
-
-        tvSignUp.setOnClickListener {
-            // TODO: Navigate to SignupActivity
-        }
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.firstpage)
+//
+////        val btnGuest = findViewById<Button>(R.id.btnGuest)
+////        val btnLogin = findViewById<Button>(R.id.btnLogin)
+////        val tvSignUp = findViewById<TextView>(R.id.tvSignUp)
+//
+////        btnGuest.setOnClickListener {
+////            // TODO: Navigate as guest
+////        }
+////
+////        btnLogin.setOnClickListener {
+////            // TODO: Navigate to LoginActivity
+////        }
+////
+////        tvSignUp.setOnClickListener {
+////            // TODO: Navigate to SignupActivity
+////        }
+//    }
 }
